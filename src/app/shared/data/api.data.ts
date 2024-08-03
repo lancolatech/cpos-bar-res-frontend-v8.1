7d72d67216c6981f3a5c9f59a4a734f8fc465df3
shaphankirui <79963010+shaphankirui@users.noreply.github.com>
2024-08-03 11:52:26 +0300

    CHANGES

--- src/app/shared/data/api.data.ts
+++ src/app/shared/data/api.data.ts
@@ -1,6 +1,6 @@
 // API URL FOR PRODUCTION
 export const apiLocalHostURL = 'https://backend.c-pos.co.ke/api';
-// export const apiProductionURL = 'https://cpos-bar-res-backend.vercel.app';
-export const apiProductionURL = 'http://localhost:3000';
+export const apiProductionURL = 'https://backendapi.c-pos.co.ke/';
+// export const apiProductionURL = 'http://localhost:3000';
 // export const apiProductionURL = `https://cpos-bar-res-backend.vercel.app`;
 // export const apiProductionURL = 'https://api.c-pos.co.ke/api';
